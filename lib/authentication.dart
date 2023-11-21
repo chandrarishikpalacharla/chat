eb182de6cae024c49fd047114bdf25258506fe84
Chandra rishik Palacharla <chandrarishikpalacharla@Chandras-Laptop.local>
2023-11-21 22:19:40 +0530

    first commit

---
+++ lib/authentication.dart
@@ -0,0 +1,17 @@
+import 'package:chat/homepage.dart';
+import 'package:chat/loginscreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_auth.currentUser != null) {
+      return HomeScreen();
+    } else {
+      return LoginScreen();
+    }
+  }
+}
